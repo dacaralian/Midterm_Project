1d4c119806c98adf88a65df9b5355c2433c6d51d
Daniel Meek Caralian <dacaralian@my.cspc.edu.ph>
2025-10-09 17:20:23 +0800

    updated commit

--- bsit3b_ai_bulletin/database/migrations/2025_10_08_083157_create_schedules_table.php
+++ bsit3b_ai_bulletin/database/migrations/2025_10_08_083157_create_schedules_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('schedules', function (Blueprint $table) {
@@ -23,10 +21,8 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
+    
     public function down(): void
     {
         Schema::dropIfExists('schedules');
